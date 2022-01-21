89f1981db123cf6d90748ce97b0a70228a35388b
Mustafa Atmaca <98126723+atmcmustafa@users.noreply.github.com>
2022-01-21 21:34:35 +0300

    Create months switch case.cpp

---
+++ months switch case.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+   int ay;
+
+    cout<<"Lutfen bir ay giriniz.."<<endl;
+    cin>>ay;
+
+    switch(ay){
+    case 1:
+        cout<<"Ocak ayi";
+        break;
+    }
+    switch(ay){
+    case 2:
+    cout<<"Subat ayi";
+    break;
+    }
+    switch (ay){
+case 3:
+    cout<<"mart ayi";
+    break;
+    }
+    switch(ay){
+case 4:
+    cout<<"Nisan ayi";
+    break;
+    }
+    switch(ay){
+    case 5:
+        cout<<"Mayis Ayi";
+    break;}
+switch(ay){
+case 6:
+    cout<<"Haziran ayi";
+    break;
+}
+    switch(ay){
+case 7:
+    cout<<"Temmmuz Ayi";
+    break;
+    }
+    switch(ay){
+case 8:
+    cout<<"Agustos Ayi";
+    break;
+    }
+    switch(ay){
+case 9:
+    cout<<"Eylul Ayi";
+ break;   }
+    switch(ay){
+case 10:
+    cout<<"Ekim Ayi";
+ break;   }
+    switch(ay){
+case 11:
+    cout<<"Kasim Ayi";
+break;
+    }
+    switch(ay){
+case 12:
+    cout<<"Aralik Ayi";
+break;
+    }
+
+
+
+}
