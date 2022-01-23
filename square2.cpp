41aefeb948fd3c41698d3143ab2d576af0211036
Mustafa Atmaca <98126723+atmcmustafa@users.noreply.github.com>
2022-01-23 20:12:34 +0300

    Create square2.cpp

---
+++ square2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<cstdlib>
+using namespace std;
+
+int main()
+{
+
+int n=100 ;
+cout<<"Konsolu kapatmak icin 0'a basiniz"<<endl;
+cout<<"Karesini hesaplamak istediginiz sayiyi giriniz:";
+while(n!=0){
+
+
+    cin>>n;
+
+    cout<<n<<" "<<"sayisinin karesi "<<n*n;
+
+}
+
+
+return 0;
+
+
+
+
+
+
+
+}
